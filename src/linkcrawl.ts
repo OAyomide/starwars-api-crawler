e0a50cfbe52bc6c24ed9ba484166e64de107cbb8
Ayomide <onigbindeayomide@gmail.com>
2018-12-17 19:41:54 +0100

    dummy function to cawl by category

--- src/linkcrawl.ts
+++ src/linkcrawl.ts
@@ -4,13 +4,14 @@ import * as fs from 'fs'
 import { resolve as res } from 'path'
 export default async function test() {
   try {
+
     let url: string = 'http://starwars.com/databank'
     const go: any = await axios(url);
     const body: string = go.data
     const $: any = cheerio.load(body)
     const nms: Array<object> = []
 
-    //then we want to gett all the children under the class that represents each entity we want to crawl
+    //then we want to get all the children under the class that represents each entity we want to crawl
     $('.building-block-wrapper').each( function(i, elemt){
       nms[i] = {
         'title': $(this).find('.long-title').text(),
@@ -21,17 +22,37 @@ export default async function test() {
     })
 
     const filterData = nms.filter(x => x !== undefined)
-    const starwarsResistanceFile = await writeFile('resistance.json', filterData)
-    console.log(starwarsResistanceFile)
+    await writeFile('resistance.json', filterData)
 
 
+    /**
+     * THIS IS EXPERIMENTAL. START CRAWLINF FROM THE SECTION TAG
+     */
+    const ty: Array<any> = []
+    const th: Array<any> = []
+     $('.blocks-container').each( function(i, elemt){
+      ty[i] = {
+        'title': $(this).find('h2').text(),
+        'data': $('.building-block-wrapper').each( function(i, elemt){
+          th[i] = {
+            'title': $('.building-block-wrapper').find('.long-title').text(),
+            'description': $('.building-block-wrapper').find('.desc').text(),
+            'url': $('.building-block-wrapper').find('.desc-sizer').children('a').attr('href'),
+            'image': $('.building-block-wrapper').find('.image-wrapper').children('.aspect').children('a').children('img').attr('src')
+          }
+        })    
+      }
+    })
+
+     const filterTy = ty.filter(x => x.title !== null || '')
+     console.log(`HERE IS WHERE THINGS GET BEATUIFUL 🦄`, filterTy)
   } catch (error) {
     console.log(`Error making async call`, error)
   }
 }
 
 
-const writeFile = (filename: string, data: any) => new Promise((resolve, reject) => {
+const writeFile = (filename: string, data: any) => new Promise((resolve: any, reject: any) => {
   fs.writeFile(res(__dirname, '../src/asset', filename), JSON.stringify(data, null, 4), { flag: 'w+'}, (err) => {
     if (err) {
       if (err.code === 'ENOENT') {
